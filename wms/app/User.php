392591b2bf35f04347b6f70f19b144f2872e1ce7
twxyxq <wufcbibu@163.com>
2017-03-31 11:23:08 +0800

    V1.06

--- wms/app/User.php
+++ wms/app/User.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
 
 class User extends Authenticatable
 {
@@ -26,4 +28,32 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    function __construct(array $attributes = []){
+
+        parent::__construct($attributes);
+
+        if (!Schema::hasTable("users")) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('code')->unique();
+                $table->string('name');
+                $table->string('email')->unique();
+                $table->string('auth')->nullable();
+                $table->string('password');
+                $table->rememberToken();
+                $table->timestamps();
+                $table->integer('created_by')->default(0);
+            });
+        }
+
+        if (!Schema::hasTable("password_resets")) {
+            Schema::create('password_resets', function (Blueprint $table) {
+                $table->string('email')->index();
+                $table->string('token')->index();
+                $table->timestamp('created_at')->nullable();
+            });
+        }
+    }
+
 }
