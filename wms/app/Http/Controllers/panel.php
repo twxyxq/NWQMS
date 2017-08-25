40be3674bf8907a0cdbab2de9d6ca7bb5c95c912
twxyxq <wufcbibu@163.com>
2017-08-25 22:35:05 +0800

    V2.00

--- wms/app/Http/Controllers/panel.php
+++ wms/app/Http/Controllers/panel.php
@@ -37,6 +37,37 @@ class panel extends Controller
         return $pview;
     }
 
+    function app_startup(){
+        $app = new \App\eps();
+        $app = new \App\exam();
+        $app = new \App\exam_item();
+        $app = new \App\exam_plan();
+        $app = new \App\exam_report();
+        $app = new \App\exam_sheet();
+        $app = new \App\gps();
+        $app = new \App\material_sheet();
+        $app = new \App\modify_history();
+        $app = new \App\pp();
+        $app = new \App\procedure();
+        $app = new \App\procedure_item();
+        $app = new \App\qf_range();
+        $app = new \App\qp();
+        $app = new \App\qp_proc();
+        $app = new \App\qp_proc_model();
+        $app = new \App\qualification();
+        $app = new \App\secondary_store();
+        $app = new \App\setting();
+        $app = new \App\tsk();
+        $app = new \App\User();
+        $app = new \App\validation();
+        $app = new \App\validation_plan();
+        $app = new \App\wj();
+        $app = new \App\wj_base();
+        $app = new \App\wj_model();
+        $app = new \App\wpq();
+        $app = new \App\wps();
+    }
+
 
 
     function index(){
