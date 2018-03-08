b99c9cf6d0968b3c28a8874300c51b1d148c215d
twxyxq <wufcbibu@163.com>
2018-03-08 11:12:07 +0800

    V3.12

--- wms/resources/views/interior_management/all_photo.blade.php
+++ wms/resources/views/interior_management/all_photo.blade.php
@@ -18,6 +18,9 @@
 
 @section('content')
 <div class="content">
+	图片上传方法：直接将图片发送到“图片归档”
+</div>
+<div class="content">
 	@foreach($photos as $photo)
 	<div class="col-xs-4 col-sm-3 col-md-2 col-lg-1 single_photo">
 		<a href="###" onclick="new_flavr('/console/detail?type=img&content=http://cme-csd.oss-cn-shenzhen.aliyuncs.com/{{$photo->getKey()}}')" class="thumbnail">
